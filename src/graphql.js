64fb8f2ffadeaedba809553583ae81d1718a451e
Hitesh Khandelwal <hitesh@smartprix.com>
2018-07-25 13:48:39 +0530

    fix toGqlArg and add a graphql tagged string function

--- src/graphql.js
+++ src/graphql.js
@@ -83,7 +83,7 @@ function convertSingleArgToGql(value) {
 		if (isPlainObject(value)) {
 			// recursively build it
 			// eslint-disable-next-line no-use-before-define
-			return convertObjectToGqlArg(value);
+			return `{${convertObjectToGqlArg(value)}}`;
 		}
 
 		return JSON.stringify(value);
@@ -113,6 +113,7 @@ function convertObjectToGqlArg(obj, pickProps) {
  * @param {Object} opts {pick: null, braces: false}
  * pick is an array of fields to pick from the object, other fields will be ignored
  * braces denotes whether to wrap all the fields in () or not (default: false)
+ * curly denotes whether to wrap the result in curly braces if it's an object (default: false)
  * @param  {Object} obj
  * @return {String} graphQL argument string
  */
@@ -131,6 +132,10 @@ function toGqlArg(obj, opts = {}) {
 		}
 
 		argStr = convertObjectToGqlArg(obj, opts.pick);
+
+		if (opts.curly) {
+			return `{${argStr}}`;
+		}
 	}
 
 	if (opts.braces) {
@@ -140,8 +145,48 @@ function toGqlArg(obj, opts = {}) {
 	return argStr || '# no args <>\n';
 }
 
+/**
+ * gqlTag is a graphql tag you can use while building graphql queries
+ * it can be used as:
+ * 	gqlTag`products(${args}) { ${fields} }`
+ * 	gqlTag`products(id: ${id}, name: ${name}) { id name ${fields} }`
+ *
+ * here args is an object, fields can be a string / array, id / name can be of any type
+ */
+function gqlTag(strings, ...args) {
+	let out = strings[0];
+	for (let i = 1; i < strings.length; i++) {
+		const arg = args[i - 1];
+		const matches = strings[i - 1].match(/(:|\()\s*$/);
+		if (matches) {
+			// arg is a graphql argument
+			if (matches[1] === ':') {
+				// arg is a single graphql argument
+				out += convertSingleArgToGql(arg);
+			}
+			else {
+				// arg is an object of graphql arguments
+				out += toGqlArg(arg);
+			}
+		}
+		else if (arg) {
+			// arg is a graphql field
+			if (typeof arg === 'string') {
+				out += arg;
+			}
+			else if (Array.isArray(arg)) {
+				out += arg.filter(Boolean).join(' ');
+			}
+		}
+
+		out += strings[i];
+	}
+	return out;
+}
+
 export {
 	handleGraphqlRequest,
 	toGqlArg,
+	gqlTag,
 	convertGraphql,
 };
